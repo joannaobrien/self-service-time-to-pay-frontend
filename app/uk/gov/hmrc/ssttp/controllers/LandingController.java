0595ef11c7bb4e086b24b9a3f81a1fd3414b9c87
Andrew Walters <andrew.walters@digital.hmrc.gov.uk>
2016-07-22 16:34:56 +0100

    Removing new String

--- app/uk/gov/hmrc/ssttp/controllers/LandingController.java
+++ app/uk/gov/hmrc/ssttp/controllers/LandingController.java
@@ -23,7 +23,7 @@ import views.html.landing;
 public class LandingController extends Controller {
 
     public static Result landing() {
-        return ok(landing.render(new String()));
+        return ok(landing.render(""));
     }
 
 }
