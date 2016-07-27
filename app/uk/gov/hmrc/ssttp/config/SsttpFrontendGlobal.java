064a0667a6dc02db2ad07dc7e2662103f0044bcb
Andrew Walters <andrew.walters@digital.hmrc.gov.uk>
2016-07-27 17:04:28 +0100

    Expanding upon exception tests. Refactor of code.

--- app/uk/gov/hmrc/ssttp/config/SsttpFrontendGlobal.java
+++ app/uk/gov/hmrc/ssttp/config/SsttpFrontendGlobal.java
@@ -8,50 +8,30 @@ import play.mvc.Http.RequestHeader;
 import play.mvc.Result;
 import play.twirl.api.Html;
 
+import static java.lang.String.format;
+import static play.libs.F.Promise.pure;
+import static play.mvc.Http.Status.*;
 import static play.mvc.Results.*;
 
 public class SsttpFrontendGlobal extends GlobalSettings {
 
-    public Html standardErrorTemplate(String pageTitle, String heading, String message){
-        return views.html.error_template.render(pageTitle, heading, message);
-    }
-
-    public Result badRequestTemplate(RequestHeader rh) {
-        return badRequest(standardErrorTemplate(
-            Messages.get("global.error.badRequest400.title"),
-            Messages.get("global.error.badRequest400.heading"),
-            Messages.get("global.error.badRequest400.message")));
-    }
-
-    public Result notFoundTemplate(RequestHeader rh) {
-        return notFound(standardErrorTemplate(
-            Messages.get("global.error.pageNotFound404.title"),
-            Messages.get("global.error.pageNotFound404.heading"),
-            Messages.get("global.error.pageNotFound404.message")));
-    }
-
-    public Result internalServerErrorTemplate() {
-        return internalServerError(standardErrorTemplate(
-            Messages.get("global.error.InternalServerError500.title"),
-            Messages.get("global.error.InternalServerError500.heading"),
-            Messages.get("global.error.InternalServerError500.message")));
+    private Html standardErrorTemplate(int errorCode){
+        return views.html.error_template.render(
+                Messages.get(format("global.error.%d.title", errorCode)),
+                Messages.get(format("global.error.%d.heading", errorCode)),
+                Messages.get(format("global.error.%d.message", errorCode)));
     }
 
     public Promise<Result> onBadRequest(RequestHeader rh, String error) {
-        return Promise.pure(badRequestTemplate(rh));
+        return pure(badRequest(standardErrorTemplate(BAD_REQUEST)));
     }
 
     public Promise<Result> onHandlerNotFound(RequestHeader rh) {
-        return Promise.pure(notFoundTemplate(rh));
+        return pure(notFound(standardErrorTemplate(NOT_FOUND)));
     }
 
     public Promise<Result> onError(RequestHeader rh, Throwable t) {
-
-        return Promise.pure(resolveError(t));
-    }
-
-    public Result resolveError(Throwable t) {
-        Logger.error(t.getMessage()+t.getCause());
-        return internalServerErrorTemplate();
+        Logger.error(t.getMessage(), t);
+        return pure(internalServerError(standardErrorTemplate(INTERNAL_SERVER_ERROR)));
     }
 }
--- test/uk/gov/hmrc/ssttp/config/SsttpFrontendGlobalTest.java
+++ test/uk/gov/hmrc/ssttp/config/SsttpFrontendGlobalTest.java
@@ -1,20 +1,47 @@
 package uk.gov.hmrc.ssttp.config;
 
 import org.junit.Test;
+import play.mvc.Http;
 import play.mvc.Result;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static play.test.Helpers.*;
 
 public class SsttpFrontendGlobalTest extends SsttpFrontendGlobal {
+    private Http.RequestHeader rh = mock(Http.RequestHeader.class);
+    private int futureTimeout = 3000;
 
     @Test
     public void renderInternalServerError() {
         running(fakeApplication(), () -> {
             Exception exception = new Exception("Runtime exception");
-            Result result = resolveError(exception);
+            Result result = onError(rh, exception).get(futureTimeout);
             assertThat(status(result), is(INTERNAL_SERVER_ERROR));
+            assertThat(contentAsString(result), containsString("Sorry, we’re experiencing technical difficulties"));
+            assertThat(contentAsString(result), containsString("Please try again in a few minutes"));
+        });
+    }
+
+    @Test
+    public void renderPageNotFound() {
+        running(fakeApplication(), () -> {
+            Result result = onHandlerNotFound(rh).get(futureTimeout);
+            assertThat(status(result), is(NOT_FOUND));
+            assertThat(contentAsString(result), containsString("This page can’t be found"));
+            assertThat(contentAsString(result), containsString("Please check that you have entered the correct web address"));
+        });
+    }
+
+    @Test
+    public void renderBadRequest() {
+        running(fakeApplication(), () -> {
+            Result result = onBadRequest(rh, "").get(futureTimeout);
+            assertThat(status(result), is(BAD_REQUEST));
+            assertThat(contentAsString(result), containsString("Bad request"));
+            assertThat(contentAsString(result), containsString("Please check that you have entered the correct web address"));
         });
     }
 }
